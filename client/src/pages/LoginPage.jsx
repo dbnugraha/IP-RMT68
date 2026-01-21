f97ef419ac185eb9a493c4fce30499b2e9565f6d
dbnugraha <dbnugrahajs@students.hacktiv8.ac.id>
2026-01-21 22:04:48 +0700

    client planning

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect } from "react";
 
 export default function LoginPage() {
+  const handleCredentialResponse = (response) => {
+    // console.log("Encoded JWT ID token: " + response.credential);
+    console.log(response.credential);
+  };
   useEffect(() => {
     google.accounts.id.initialize({
       client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
       callback: handleCredentialResponse,
     });
-    google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large" }, // customization attributes
-    );
-    google.accounts.id.prompt(); // also display the One Tap dialog
+    google.accounts.id.renderButton(document.getElementById("buttonDiv"), { theme: "outline", size: "large" });
+    google.accounts.id.prompt();
   }, []);
 
   return (
